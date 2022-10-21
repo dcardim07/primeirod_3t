3bc0cb5c0ad8a40b43f8bdf38d45412c7cda7b3d
dcardim07 <106184648+dcardim07@users.noreply.github.com>
2022-10-21 15:13:37 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,30 +1,43 @@
-let tabuada = 6;
 
-function escreva(){
-    document.write(tabuada + " x 1 = "+(tabuada*1)+"<br>");
-    document.write(tabuada + " x 2 = "+(tabuada*2)+"<br>");
-    document.write(tabuada + " x 3 = "+(tabuada*3)+"<br>");
-    document.write(tabuada + " x 4 = "+(tabuada*4)+"<br>");
-    document.write(tabuada + " x 5 = "+(tabuada*5)+"<br>");
-    document.write(tabuada + " x 6 = "+(tabuada*6)+"<br>");
-    document.write(tabuada + " x 7 = "+(tabuada*7)+"<br>");
-    document.write(tabuada + " x 8 = "+(tabuada*8)+"<br>");
-    document.write(tabuada + " x 9 = "+(tabuada*9)+"<br>");
-    document.write(tabuada + " x 10 = "+(tabuada*10)+"<br>");
+function moeda(atual){
+return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
 }
 
-function repete(){
-    for(let i = 1; i <= 10; i++){
-        document.write("Tabuada do " + i + "<br>");
-        for(let j =1; j <= 10; j++){
-            document.write(i + " x " + j + " = "+(j*i)+"<br>");
-        }
-        document.write("<br>");
-    }
-}
+function total(){
+  let c = document.getElementById("valor").value;
+  let j = document.getElementById("juros").value;
+  let t = document.getElementById("meses").value;
+  let r = moeda;
 
-let lista = ["Banana","Uva","Abacate","Tomate"];
 
-function minhaLista(){
-    document.write(lista[3]);
-}
+  if(!Number(c)){
+      
+   alert("O capital deve ser numeros.");
+   document.getElementById("valor").value = "";
+   document.getElementById("valor").focus();
+   return;
+  }
+
+  if(!Number(j)){   
+    alert("O juros deve ser numeros.");
+    document.getElementById("juros").value = "";
+    document.getElementById("juros").focus();
+    return;
+   }
+
+   if(!Number(t)){
+    alert("O meses deve ser numeros.");
+    document.getElementById("meses").value = "";
+    document.getElementById("meses").focus();
+    return;
+   }
+  }
+
+  for(let i = 1; i <= t; i++){
+      r = c * (1 + (j/100));
+      document.write("Mês " + i + " valor: " + moeda(r) +"<br>");
+      c = moeda(r);
+  }
+  document.write("Resultado: " + moeda(r));
+
+  
